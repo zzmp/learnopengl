31f70d5b7c3b647e12cc7fa9aa0ff9a9d3a04403
Zach Pomerantz <zmp@umich.edu>
2015-12-03 16:13:28 -0500

    Getting Started > Shaders > Attributes

--- shaders.cc
+++ shaders.cc
@@ -7,23 +7,26 @@ const std::string vertexShaderSource = R"(
 #version 330 core
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 color;
+
+out vec3 ourColor;
 
 void main()
 {
     gl_Position = vec4(position, 1.0);
+    ourColor = color;
 }
 )";
 
 const std::string fragmentShaderSource = R"(
 #version 330 core
 
+in vec3 ourColor;
 out vec4 color;
-
-uniform vec4 ourColor;
 
 void main()
 {
-    color = ourColor;
+    color = vec4(ourColor, 1.0f);
 }
 )";
 
@@ -44,9 +47,10 @@ int main() {
     program.link();
 
 		GLfloat vertices[] = {
-      -0.5f, -0.5f, 0.0f,
-       0.5f, -0.5f, 0.0f,
-       0.0f,  0.5f, 0.0f
+      // Positions        Colors
+       0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+       0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f
     };
 
 		GLuint VAO;
@@ -58,8 +62,13 @@ int main() {
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+    // Positions
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
+
+    // Colors
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+		glEnableVertexAttribArray(1);
 
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
@@ -68,15 +77,9 @@ int main() {
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
 
-      // Get the uniform color
-      GLfloat timeValue = glfwGetTime();
-      GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
-      GLint vertexColorLocation = program.getUniform("ourColor");
-
 			// Draw a triangle
 			glBindVertexArray(VAO);
       program.use();
-      glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
       glDrawArrays(GL_TRIANGLES, 0, 3);
 			glBindVertexArray(0);
     });
